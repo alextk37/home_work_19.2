e97ae0f91baeceed9ea239af8aeebe4a52e3e809
alextk37 <alextk37@yandex.ru>
2024-07-10 12:15:50 +0300

    settings admin panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Category, Products
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "category_name")
+    search_fields = ("category_name",)
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ("id", "product_name", "price", "category")
+    search_fields = ("product_name", "description")
+    list_filter = ("category",)
