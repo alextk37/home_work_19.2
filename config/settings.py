acf5efe1470cdb59c6ec1584b3ee965e36596611
alextk37 <alextk37@yandex.ru>
2024-07-10 09:24:31 +0300

    add connection db settings.py

--- config/settings.py
+++ config/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "web_app",
+        "USER": "postgres",
+        "PORT": 5438,
+        "PASSWORD": "198647gaM",
+        "HOST": "127.0.0.1"
     }
 }
 
