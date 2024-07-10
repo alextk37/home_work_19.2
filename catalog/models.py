9f1eceed6f733d9a699cec76e2d792fa85993819
alextk37 <alextk37@yandex.ru>
2024-07-10 10:24:41 +0300

    create model Products

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {"null": True, "blank": True}
+
+class Products(models.Model):
+    product_name = models.CharField(max_length=225, verbose_name="продукт")
+    description = models.TextField(verbose_name="описание")
+    preview_img = models.ImageField(uplpad_to="product_images", verbose_name='Изображение', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.IntegerField(verbose_name='Цена')
+    created_at = models.DateField(verbose_name='Дата создания', auto_now=True)
+    updated_at = models.DateField(verbose_name='Дата создания', auto_now=True)
+
+    def __str__(self):
+        return f"{self.product_name}"
+
+    class Meta:
+        verbose_name = "продукт"
+        verbose_name_plural = "продукты"
+        ordering = ("product_name",)
+
