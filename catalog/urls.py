bfadffe4cee2fde4917ec11f0509f7a0e43ce36f
alextk37 <alextk37@yandex.ru>
2024-07-15 22:56:46 +0300

    add index view in urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import catalog, contacts
+from catalog.views import catalog, contacts, index
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", catalog, name="catalog"),
+    path("", index, name="index"),
+    path("catalog", catalog, name="catalog"),
     path("contacts", contacts, name="contacts")
 ]
