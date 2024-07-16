be9a2a0de468596ab78b77433d8544b13625fc33
alextk37 <alextk37@yandex.ru>
2024-07-16 12:29:54 +0300

    add new_product page in urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import catalog, contacts, index, product_item
+from catalog.views import catalog, contacts, index, product_item, new_product
 
 app_name = CatalogConfig.name
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path("catalog", catalog, name="catalog"),
     path("contacts", contacts, name="contacts"),
     path("product_item/<int:pk>", product_item, name="product_item"),
+    path("new_product", new_product, name="new_product"),
 ]
