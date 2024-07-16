e5555ca0619c803c3a6e7a9c9ee23a99d23cc9ff
alextk37 <alextk37@yandex.ru>
2024-07-16 12:29:31 +0300

    add new_product view

--- catalog/views.py
+++ catalog/views.py
@@ -51,3 +51,27 @@ def product_item(request, pk):
         "title": Products.objects.get(pk=pk).product_name,
     }
     return render(request, "product_item.html", context)
+
+def new_product(request):
+    context = {
+        "title": "Новый продукт",
+        "categories": Category.objects.all(),
+    }
+    if request.method == "POST":
+        product_name = request.POST.get("productName")
+        description = request.POST.get("productDescription")
+        preview_img = request.FILES.get("productImage")
+        category_id = request.POST.get("productCategory")
+        price = request.POST.get("productPrice")
+
+        category = Category.objects.get(pk=category_id)
+
+        new_product = Products(
+            product_name=product_name,
+            description=description,
+            preview_img=preview_img,
+            category=category,
+            price=price
+        )
+        new_product.save()
+    return render(request, "new_product.html", context)
