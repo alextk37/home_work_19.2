153dddeed05f5a95cb5347781032585a93379732
alextk37 <alextk37@yandex.ru>
2024-07-16 11:07:47 +0300

    add product_item url

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import catalog, contacts, index
+from catalog.views import catalog, contacts, index, product_item
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", index, name="index"),
     path("catalog", catalog, name="catalog"),
-    path("contacts", contacts, name="contacts")
+    path("contacts", contacts, name="contacts"),
+    path("product_item/<int:pk>", product_item, name="product_item"),
 ]
