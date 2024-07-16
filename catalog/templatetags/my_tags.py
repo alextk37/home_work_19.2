fdf0c7e5e27ce2fa9137867c2458502eca759485
alextk37 <alextk37@yandex.ru>
2024-07-16 15:46:04 +0300

    create user tags file

---
+++ catalog/templatetags/my_tags.py
@@ -0,0 +1,9 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def media(data):
+    if data:
+        return f"/media/{data}"
+    return "/static/img/image_cap.jpg"
