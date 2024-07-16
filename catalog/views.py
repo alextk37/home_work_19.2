a019dc7565592ee173f9fd13a87596c58bee297a
alextk37 <alextk37@yandex.ru>
2024-07-16 11:07:08 +0300

    add product_item view & add titles for pages in context

--- catalog/views.py
+++ catalog/views.py
@@ -36,9 +36,18 @@ def contacts(request):
         name = request.POST.get("name")
         email = request.POST.get("email")
         message = request.POST.get("message")
-        
+    context = {
+        "title": "Контакты",        
+    }    
 
-    return render(request, "contacts.html")
+    return render(request, "contacts.html", context)
 
 def about_company(request):
     return render(request, "about_company.html")
+
+def product_item(request, pk):
+    context = {
+        "product": Products.objects.get(pk=pk),
+        "title": Products.objects.get(pk=pk).product_name,
+    }
+    return render(request, "product_item.html", context)
