838ed2bc1c114348c4fedd4755faca9e8bd625ff
alextk37 <alextk37@yandex.ru>
2024-07-24 17:28:15 +0300

    add text tag

--- blog/templatetags/my_tags.py
+++ blog/templatetags/my_tags.py
@@ -8,3 +8,10 @@ def media(data):
     if data:
         return f"/media/{data}"
     return "/static/img/image_cap.jpg"
+
+
+@register.filter
+def text(data):
+    if data:
+        return data
+    return "Текст отсутствует"
