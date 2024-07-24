49a1fd4a8066ff6165c5090e42ca97d4f335e303
alextk37 <alextk37@yandex.ru>
2024-07-24 13:23:00 +0300

    add Article model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {"null": True, "blank": True}
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=225, verbose_name="заголовок")
+    description = models.TextField(verbose_name="описание")
+    content = models.TextField(verbose_name="содержание")
+    is_published = models.BooleanField(verbose_name="Опубликовано", default=False)
+    preview_img = models.ImageField(
+        upload_to="article_images", verbose_name="Изображение", **NULLABLE
+    )
+    created_at = models.DateField(
+        verbose_name="Дата создания", auto_now=True, **NULLABLE
+    )
+    updated_at = models.DateField(
+        verbose_name="Дата изменения", auto_now=True, **NULLABLE
+    )
+    view_count = models.IntegerField(verbose_name="Количество просмотров", default=0)
+    slug = models.SlugField(max_length=255, unique=True, **NULLABLE)
+
+    class Meta:
+        verbose_name = "статья"
+        verbose_name_plural = "статьи"
+        ordering = ("title",)
+
+    def __str__(self):
+        return self.title
