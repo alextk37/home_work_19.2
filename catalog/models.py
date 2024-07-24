8c334d8d26ecf8906b089ae666c57b87fc364f0d
alextk37 <alextk37@yandex.ru>
2024-07-24 09:54:08 +0300

    fix import

--- catalog/models.py
+++ catalog/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.db.models import Count
 
 NULLABLE = {"null": True, "blank": True}
 
