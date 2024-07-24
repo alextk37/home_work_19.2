28e3fad2a1f9de2b76e88edaef1decddcfecfd5a
alextk37 <alextk37@yandex.ru>
2024-07-24 12:24:57 +0300

    refactor urls on CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import catalog, contacts, index, product_item, new_product
+from catalog.views import (
+    index,
+    CatalogListView,
+    CatalogDetailView,
+    CatalogCreateView,
+    ContactsView,
+)
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", index, name="index"),
-    path("catalog", catalog, name="catalog"),
-    path("contacts", contacts, name="contacts"),
-    path("product_item/<int:pk>", product_item, name="product_item"),
-    path("new_product", new_product, name="new_product"),
+    path("catalog", CatalogListView.as_view(), name="catalog"),
+    path("contacts", ContactsView.as_view(), name="contacts"),
+    path("product_item/<int:pk>", CatalogDetailView.as_view(), name="product_item"),
+    path("new_product", CatalogCreateView.as_view(), name="new_product"),
 ]
