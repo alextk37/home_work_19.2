dbbb3beae6af30a2beb9a07807fc9e3a371e0828
alextk37 <alextk37@yandex.ru>
2024-08-11 15:01:43 +0300

    add stop words validation

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,11 +1,29 @@
 from django import forms
 from catalog.models import Products
+from django.core.exceptions import ValidationError
+
+STOP_WORDS = [
+    "казино",
+    "криптовалюта",
+    "крипта",
+    "биржа",
+    "дешево",
+    "бесплатно",
+    "обман",
+    "полиция",
+    "радар",
+]
 
 
 class CatalogCreateForm(forms.ModelForm):
     class Meta:
         model = Products
-        exclude = (
-            "view_count",
-            "slug",
-        )
+        fields = ("product_name", "description", "preview_img", "category", "price")
+
+    def clean_product_name(self):
+        cleaned_data = super().clean()["product_name"]
+
+        if cleaned_data in STOP_WORDS:
+            raise ValidationError("Недопустимое имя продукта")
+        else:
+            return cleaned_data
