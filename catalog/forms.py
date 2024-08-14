69c4bee676fd911eb47745d7b33a3fd90a3c48da
alextk37 <alextk37@yandex.ru>
2024-08-14 15:14:21 +0300

    add forms for create & update product

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from catalog.models import Products
+from catalog.models import Products, Version
 from django.core.exceptions import ValidationError
 
 STOP_WORDS = [
@@ -15,10 +15,14 @@ STOP_WORDS = [
 ]
 
 
-class CatalogCreateForm(forms.ModelForm):
-    class Meta:
-        model = Products
-        fields = ("product_name", "description", "preview_img", "category", "price")
+class FormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs["class"] = "form-control"
+            print(field_name)
+            if field_name == "is_active":
+                field.widget.attrs["class"] = "form-check-input"
 
     def clean_product_name(self):
         cleaned_data = super().clean()["product_name"]
@@ -27,3 +31,21 @@ class CatalogCreateForm(forms.ModelForm):
             raise ValidationError("Недопустимое имя продукта")
         else:
             return cleaned_data
+
+
+class CatalogCreateForm(FormMixin, forms.ModelForm):
+    class Meta:
+        model = Products
+        fields = ("product_name", "description", "preview_img", "category", "price")
+
+
+class CatalogUpdateForm(FormMixin, forms.ModelForm):
+    class Meta:
+        model = Products
+        fields = ("product_name", "description", "preview_img", "category", "price")
+
+
+class VersionForm(FormMixin, forms.ModelForm):
+    class Meta:
+        model = Version
+        fields = ("version", "name_version", "is_active")
