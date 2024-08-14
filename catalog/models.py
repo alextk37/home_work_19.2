c5ba2c4bdc803fb9f0c43e1b92f1aad9ab3b75e0
alextk37 <alextk37@yandex.ru>
2024-08-14 15:13:49 +0300

    add Version model

--- catalog/models.py
+++ catalog/models.py
@@ -35,7 +35,7 @@ class Products(models.Model):
 class Category(models.Model):
     category_name = models.CharField(max_length=225, verbose_name="категория")
     description = models.TextField(verbose_name="описание")
-    has_products = models.BooleanField(verbose_name="Есть продукты", default=False)
+    has_products = models.BooleanField(verbose_name="Есть продукты")
 
     def __str__(self):
         return f"{self.category_name}"
@@ -44,3 +44,29 @@ class Category(models.Model):
         verbose_name = "категория"
         verbose_name_plural = "категории"
         ordering = ("category_name",)
+
+
+class Version(models.Model):
+    product = models.ForeignKey(
+        Products,
+        on_delete=models.DO_NOTHING,
+        verbose_name="продукт",
+        **NULLABLE,
+        related_name="versions",
+    )
+    version = models.IntegerField(verbose_name="номер версии")
+    name_version = models.CharField(
+        verbose_name="имя версии", **NULLABLE, max_length=225
+    )
+    created_at = models.DateField(
+        verbose_name="дата создания", auto_now=True, **NULLABLE
+    )
+    is_active = models.BooleanField(verbose_name="Активна", default=False)
+
+    def __str__(self):
+        return f"{self.version}"
+
+    class Meta:
+        verbose_name = "версия"
+        verbose_name_plural = "версии"
+        ordering = ("version",)
