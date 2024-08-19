25f61ffa983bc02fcd3a2101fcd935e74a002d3b
alextk37 <alextk37@yandex.ru>
2024-08-19 11:50:56 +0300

    add user castom model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {"null": True, "blank": True}
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Поле Email должно быть установлено")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, verbose_name="email")
+    phone = models.CharField(max_length=20, verbose_name="телефон", **NULLABLE)
+    avatar = models.ImageField(upload_to="users/", verbose_name="аватар", **NULLABLE)
+    country = models.CharField(max_length=100, verbose_name="страна", **NULLABLE)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "пользователь"
+        verbose_name_plural = "пользователи"
