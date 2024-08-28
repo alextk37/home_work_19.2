f6d2ca1da40c850b2cff5ac2283d4f15bfa94565
alextk37 <alextk37@yandex.ru>
2024-08-28 11:42:09 +0300

    add permission in products model

--- catalog/models.py
+++ catalog/models.py
@@ -38,6 +38,14 @@ class Products(models.Model):
         verbose_name = "продукт"
         verbose_name_plural = "продукты"
         ordering = ("product_name",)
+        permissions = [
+            ("can_change_is_published", "Может редактировать статус публикации"),
+            (
+                "can_change_product_description",
+                "Может редактировать количество описание",
+            ),
+            ("can_change_product_category", "Может редактировать категорию"),
+        ]
 
 
 class Category(models.Model):
