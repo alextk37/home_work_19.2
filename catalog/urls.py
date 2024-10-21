ca9615e6c3cbde6df645b534e311f8b03d558f1a
Alex Gorbunov <alextk37@yandex.ru>
2024-10-21 14:06:28 +0300

    add urls categories

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,7 +7,9 @@ from catalog.views import (
     CatalogCreateView,
     ContactsView,
     CatalogUpdateView,
+    category_list_view
 )
+from  django.views.decorators.cache import cache_page
 
 app_name = CatalogConfig.name
 
@@ -15,7 +17,7 @@ urlpatterns = [
     path("", index, name="index"),
     path("catalog", CatalogListView.as_view(), name="catalog"),
     path("contacts", ContactsView.as_view(), name="contacts"),
-    path("product_item/<int:pk>", CatalogDetailView.as_view(), name="product_item"),
+    path("product_item/<int:pk>", cache_page(600)(CatalogDetailView.as_view()), name="product_item"),
     path("new_product", CatalogCreateView.as_view(), name="new_product"),
     path("update_product/<int:pk>", CatalogUpdateView.as_view(), name="update_product"),
-]
+    path("categories/", category_list_view, name='category_list' )]
